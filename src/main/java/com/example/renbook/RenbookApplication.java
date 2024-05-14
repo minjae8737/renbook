689cc5cb73b920615bee0b4b2fd64e7e37e408a0
Aljae <minjae8737@gmail.com>
2024-05-14 09:58:53 +0900

    Initial commit

---
+++ src/main/java/com/example/renbook/RenbookApplication.java
@@ -0,0 +1,13 @@
+package com.example.renbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RenbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/renbook/RenbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.renbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
